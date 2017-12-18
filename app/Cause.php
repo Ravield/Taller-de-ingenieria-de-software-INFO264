052804f86b0a50c41463fc0d717d8f0e7f1912b3
eidepozo <eidepozo@hotmail.com>
2017-12-18 04:12:08 -0300

    modificacion modelo causa

--- app/Cause.php
+++ app/Cause.php
@@ -8,7 +8,7 @@ class Cause extends Model
 {
   //protected $table = 'nombre_tabla';  en caso de que la tabla no tuviera el nombre en plural
   protected $primaryKey = 'id';//indicar id
-  protected $fillable = ['id', 'nombre','tipo','resumen','user_id'];
+  protected $fillable = ['id', 'nombre','tipo','resumen'];
   /*$causes = App\Cause::all();
 
   foreach ($causes as $cause) {
