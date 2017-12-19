37be434b2805b4dc38465594084803ae72aac536
eidepozo <eidepozo@hotmail.com>
2017-12-18 23:59:01 -0300

    se adjunta documentacion final

--- resources/views/layouts/adminLayout.blade.php
+++ resources/views/layouts/adminLayout.blade.php
@@ -139,7 +139,7 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="admin/img/user2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">Nombre Usuario</span>
+              <span class="hidden-xs">Elliot Ide Pozo</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
@@ -147,8 +147,8 @@
                 <img src="admin/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                 <p>
-                  Nombre usuario - Abogado
-                  <small>Member since Nov. 2012</small>
+                  Elliot Ide Pozo - Administrador
+                  <small>Miembro desde Nov. 2017</small>
                 </p>
               </li>
               <!-- Menu Body -->
@@ -172,7 +172,7 @@
                   <a href="#" class="btn btn-default btn-flat">Perfil</a>
                 </div>
                 <div class="pull-right">
-                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                  <a href="#" class="btn btn-default btn-flat">Log out</a>
                 </div>
               </li>
             </ul>
@@ -198,7 +198,7 @@
           <img src="admin/img/user2-160x160.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>Nombre Usuario</p>
+          <p>Elliot Ide Pozo</p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
